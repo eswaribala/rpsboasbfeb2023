06a0c5f0a46e84248b51f13552a39a6045a8a10a
eswaribala <parameswaribala@gmail.com>
2023-02-13 17:43:33 +0530

    updated

---
+++ customerapi/customerapi/src/main/java/com/boa/customerapi/services/IndividualService.java
@@ -0,0 +1,43 @@
+package com.boa.customerapi.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.boa.customerapi.models.Individual;
+import com.boa.customerapi.repositories.IndividualRepo;
+
+@Service
+public class IndividualService {
+    @Autowired 
+	private IndividualRepo individualRepo;
+    
+    //insert
+    
+    public Individual addIndividual(Individual individual) {
+    	
+    	return this.individualRepo.save(individual);
+    }
+    
+    //all the records
+    
+    public List<Individual> getAllIndividuals(){
+    	return this.individualRepo.findAll();
+    }
+    
+    public Individual getIndividualById(long customerId) {
+    	return this.individualRepo.findById(customerId).orElse(null);
+    }
+    
+    public boolean deleteIndividualById(long customerId) {
+    	this.individualRepo.deleteById(customerId);
+    	if(getIndividualById(customerId)==null)
+    		return true;    	
+    	else
+    		return false;
+    	
+    }
+    
+	
+}
