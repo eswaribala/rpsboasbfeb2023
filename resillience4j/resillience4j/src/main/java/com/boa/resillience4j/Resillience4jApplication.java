cd9a374a38a57e5da37ffeb1bd0d84b566efec53
eswaribala <parameswaribala@gmail.com>
2023-02-17 10:53:49 +0530

    updated

--- resillience4j/resillience4j/src/main/java/com/boa/resillience4j/Resillience4jApplication.java
+++ resillience4j/resillience4j/src/main/java/com/boa/resillience4j/Resillience4jApplication.java
@@ -2,12 +2,19 @@ package com.boa.resillience4j;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Resillience4jApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Resillience4jApplication.class, args);
+	}
+	
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 
 }
---
+++ resillience4j/resillience4j/src/main/java/com/boa/resillience4j/services/ResillienceService.java
@@ -0,0 +1,47 @@
+package com.boa.resillience4j.services;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class ResillienceService {
+
+	@Value("${serviceUrl}")
+    private String serviceUrl;
+    @Value("${alternativeServiceUrl}")
+    private String alternativeServiceUrl;
+    @Autowired
+    private RestTemplate restTemplate;
+    
+
+    @CircuitBreaker(name = "gatewayCircuitBreaker", 
+    		fallbackMethod = "fallback")
+    @Retry(name = "gatewayRetry")
+
+	public String getData(String userName,String userPwd) {
+
+        log.info(" Making a request to " + serviceUrl + " at :"+ LocalDateTime.now());
+       
+        return restTemplate.getForObject(serviceUrl+"?userName="+userName+"&userPwd="+userPwd, String.class);
+
+	}
+	
+    public String fallbackGetData(String userName,String userPwd,Exception ex) {
+    	log.info(" Making a request to " + serviceUrl + " at :"+ LocalDateTime.now());
+        
+        return restTemplate.getForObject(alternativeServiceUrl, String.class);
+
+	}
+	
+	
+}
