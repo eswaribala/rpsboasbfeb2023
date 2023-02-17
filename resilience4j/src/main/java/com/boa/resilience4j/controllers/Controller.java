56d92a8ed16a4c7b98b653ac33cbf8a04daee57f
eswaribala <parameswaribala@gmail.com>
2023-02-17 11:57:57 +0530

    updated

--- resilience4j/src/main/java/com/boa/resilience4j/controllers/Controller.java
+++ resilience4j/src/main/java/com/boa/resilience4j/controllers/Controller.java
@@ -2,6 +2,7 @@ package com.boa.resilience4j.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.boa.resilience4j.services.Service;
@@ -13,7 +14,7 @@ public class Controller {
     private Service service;
 
     @GetMapping({"/{userName}/{userPwd}"})
-    public String getValues(String userName,String userPwd) {
+    public String getValues(@PathVariable("userName") String userName, @PathVariable("userPwd") String userPwd) {
         return service.fetchData(userName,userPwd);
     }
 }
