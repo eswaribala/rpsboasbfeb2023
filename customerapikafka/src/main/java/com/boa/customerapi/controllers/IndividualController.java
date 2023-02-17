ce1fc262a17410027c192eb211cc7182d39168f1
eswaribala <parameswaribala@gmail.com>
2023-02-17 15:37:24 +0530

    updated

--- customerapikafka/src/main/java/com/boa/customerapi/controllers/IndividualController.java
+++ customerapikafka/src/main/java/com/boa/customerapi/controllers/IndividualController.java
@@ -3,11 +3,15 @@ package com.boa.customerapi.controllers;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+import com.boa.customerapi.models.Individual;
 import com.boa.customerapi.services.IndividualService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -21,5 +25,18 @@ public class IndividualController {
 	private IndividualService individualService;
   
   
+    @PostMapping({"/v1.0/"})
+    @CrossOrigin("*")
+    public ResponseEntity<?> publishIndividual(@RequestBody Individual individual){
+    
+    	
+    	if(this.individualService.publishCustomer(individual)) 
+    		return ResponseEntity.status(HttpStatus.CREATED).body("Message Published....");
+    	
+    	else
+    	 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Individual "
+    	 		+ "message not published");	
+    	
+    }
     
 }
--- customerapikafka/src/main/java/com/boa/customerapi/services/IndividualService.java
+++ customerapikafka/src/main/java/com/boa/customerapi/services/IndividualService.java
@@ -1,19 +1,44 @@
 package com.boa.customerapi.services;
 
+import java.time.LocalDate;
 import java.util.List;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.MimeTypeUtils;
 
+import com.boa.customerapi.facades.CustomerMessage;
 import com.boa.customerapi.models.Individual;
 
 
 @Service
 public class IndividualService {
   
-    
+	@Autowired
+    private CustomerMessage customerMessage;
    
     
+  public boolean publishCustomer(Individual individual) {
+		
+		MessageChannel messageChannel=customerMessage.customerPublish();
+		if(individual.getDob().isBefore(LocalDate.now())) {
+		return messageChannel.send(MessageBuilder
+                .withPayload(individual)
+                .setHeader(MessageHeaders.CONTENT_TYPE, 
+                		MimeTypeUtils.APPLICATION_JSON)
+                .build());
+		}
+		else
+		{
+			return false;
+		}
+
+		
+	}
+
 	
 }
